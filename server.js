3d0cf6db625dd7123b17aec868446468185d4406
Etherless <pabloimrik17@gmail.com>
2017-10-21 20:03:24 +0200

    404 (not found) route

--- server.js
+++ server.js
@@ -40,8 +40,29 @@ app.post('/contact', function(req, res) {
     res.send("Hola "+ req.body.name);
 });
 
-app.get('/:username', checkName, function(req, res) {
+app.get('/profile/:username', checkName, function(req, res) {
     res.send('You are ' + req.params.username);
+});
+
+// 404
+app.use(function(req, res, next) {
+    res.status(404);
+
+    // respond with html page
+    if (req.accepts('html')) {
+        res.sendFile(__dirname + '/404.html');
+        return;
+    }
+
+    // respond with json
+    if (req.accepts('json')) {
+        res.send({ error: '404 Not found' });
+        return;
+    }
+
+    // default to plain-text. send()
+    res.type('txt').send('404 Not found');
+
 });
 
 function checkName(req, res, next) {
