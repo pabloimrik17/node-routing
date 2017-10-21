e4b86ee376a2a3d8e1a8b13e87515a1bb3ba6e26
Etherless <pabloimrik17@gmail.com>
2017-10-21 19:30:45 +0200

    Contact page Post route for contact

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 var app = require('express')();
 
-port = process.env.PORT | 8080;
-morgan = require('morgan');
+var port = process.env.PORT | 8080;
+var morgan = require('morgan');
+var bodyParser = require('body-parser');
 
 // configure
 app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({extended: true}));
+
 //routes
 app.get('/', function(req, res) {
     res.sendFile(__dirname + '/index.html');
@@ -14,8 +17,24 @@ app.get('/about', function(req, res) {
     res.json({ message:'look im the about page' });
 });
 
+/*
+app.route('/contact')
+    .get(function(req, res) {
+        res.sendFile(__dirname+'/contact.html');
+    })
+    .post(function(req, res) {
+        console.log(req.body);
+        res.send("Hola "+ req.body.name);
+    });
+ */
+
 app.get('/contact', function(req, res) {
-    res.send('look im the contact page');
+    res.sendFile(__dirname+'/contact.html');
+});
+
+app.post('/contact', function(req, res) {
+    console.log(req.body);
+    res.send("Hola "+ req.body.name);
 });
 
 //start
